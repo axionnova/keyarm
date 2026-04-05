7fce38c9464e87a3790a562d06d9d56726cdb3b3
Axion Nova <leverrier940@gmail.com>
2026-04-05 19:02:12 -0300

    fix

--- hash/sha256_sse.cpp
+++ hash/sha256_sse.cpp
@@ -15,124 +15,30 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "sha256.h"
-#if defined(__arm__)
-#pragma GCC target ("fpu=crypto-neon-fp-armv8")
-#elif defined(__aarch64__)
-#pragma GCC target ("arch=armv8-a+crypto")
-#endif
+#include "sha256.h" // Inclui a versão C de SHA256
+#include <arm_neon.h> // Ainda precisamos de NEON para outras partes, se houver
+
+// Desabilitar temporariamente as pragmas de target para ARMv8 Crypto para testar o fallback
+// #if defined(__arm__)
+// #pragma GCC target ("fpu=crypto-neon-fp-armv8")
+// #elif defined(__aarch64__)
+// #pragma GCC target ("arch=armv8-a+crypto")
+// #endif
 
 #if (defined(__x86_64__) || defined(_M_X64)) && !defined(__arm__) && !defined(__aarch64__)
 #include <immintrin.h>
-#elif defined(__aarch64__) || defined(__arm__)
-#include <arm_neon.h>
 #endif
 #include <string.h>
 #include <stdint.h>
 
 #if defined(__aarch64__) || defined(__arm__)
 
-// Reverse lanes in a uint32x4_t: [0,1,2,3] -> [3,2,1,0]
-static inline uint32x4_t vrev_u32(uint32x4_t x) {
-    return vcombine_u32(vrev64_u32(vget_high_u32(x)), vrev64_u32(vget_low_u32(x)));
+// Fallback para SHA256 em C
+static inline void sha256_block_fallback(uint32_t *state, const uint32_t *data) {
+    _sha256::Transform(state, (const unsigned char*)data);
 }
 
-// ARMv8 Crypto SHA256 Implementation
-static inline void sha256_armv8_block(uint32_t *state, const uint32_t *data) {
-    // ARMv8 SHA256 instructions expect state in reversed word order:
-    // abcd lanes: [3]=A, [2]=B, [1]=C, [0]=D
-    // efgh lanes: [3]=E, [2]=F, [1]=G, [0]=H
-    uint32x4_t abcd = vrev_u32(vld1q_u32(&state[0]));
-    uint32x4_t efgh = vrev_u32(vld1q_u32(&state[4]));
-    
-    // Save original state for final addition (SHA256 standard)
-    uint32x4_t abcd_orig = abcd;
-    uint32x4_t efgh_orig = efgh;
-    
-    uint32x4_t w0 = vld1q_u32(&data[0]);
-    uint32x4_t w1 = vld1q_u32(&data[4]);
-    uint32x4_t w2 = vld1q_u32(&data[8]);
-    uint32x4_t w3 = vld1q_u32(&data[12]);
-    
-    // Reverse byte order (big-endian for SHA256)
-    w0 = vreinterpretq_u32_u8(vrev32q_u8(vreinterpretq_u8_u32(w0)));
-    w1 = vreinterpretq_u32_u8(vrev32q_u8(vreinterpretq_u8_u32(w1)));
-    w2 = vreinterpretq_u32_u8(vrev32q_u8(vreinterpretq_u8_u32(w2)));
-    w3 = vreinterpretq_u32_u8(vrev32q_u8(vreinterpretq_u8_u32(w3)));
-
-    uint32x4_t k0_3 = {0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5};
-    uint32x4_t k4_7 = {0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5};
-    uint32x4_t k8_11 = {0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3};
-    uint32x4_t k12_15 = {0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174};
-    uint32x4_t k16_19 = {0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc};
-    uint32x4_t k20_23 = {0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da};
-    uint32x4_t k24_27 = {0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7};
-    uint32x4_t k28_31 = {0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967};
-    uint32x4_t k32_35 = {0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13};
-    uint32x4_t k36_39 = {0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85};
-    uint32x4_t k40_43 = {0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3};
-    uint32x4_t k44_47 = {0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070};
-    uint32x4_t k48_51 = {0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5};
-    uint32x4_t k52_55 = {0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3};
-    uint32x4_t k56_59 = {0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208};
-    uint32x4_t k60_63 = {0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
-
-    uint32x4_t msg0, msg1, msg2, msg3, tmp;
-
-    // Rounds 0-3
-    msg0 = vaddq_u32(w0, k0_3);
-    tmp = vsha256hq_u32(abcd, efgh, msg0);
-    efgh = vsha256h2q_u32(efgh, abcd, msg0);
-    abcd = tmp;
-    // Rounds 4-7
-    msg1 = vaddq_u32(w1, k4_7);
-    tmp = vsha256hq_u32(abcd, efgh, msg1);
-    efgh = vsha256h2q_u32(efgh, abcd, msg1);
-    abcd = tmp;
-    // Rounds 8-11
-    msg2 = vaddq_u32(w2, k8_11);
-    tmp = vsha256hq_u32(abcd, efgh, msg2);
-    efgh = vsha256h2q_u32(efgh, abcd, msg2);
-    abcd = tmp;
-    // Rounds 12-15
-    msg3 = vaddq_u32(w3, k12_15);
-    tmp = vsha256hq_u32(abcd, efgh, msg3);
-    efgh = vsha256h2q_u32(efgh, abcd, msg3);
-    abcd = tmp;
-
-    for (int i = 0; i < 3; i++) {
-        w0 = vsha256su0q_u32(w0, w1);
-        w0 = vsha256su1q_u32(w0, w2, w3);
-        msg0 = vaddq_u32(w0, (i==0?k16_19:(i==1?k32_35:k48_51)));
-        tmp = vsha256hq_u32(abcd, efgh, msg0);
-        efgh = vsha256h2q_u32(efgh, abcd, msg0);
-        abcd = tmp;
-
-        w1 = vsha256su0q_u32(w1, w2);
-        w1 = vsha256su1q_u32(w1, w3, w0);
-        msg1 = vaddq_u32(w1, (i==0?k20_23:(i==1?k36_39:k52_55)));
-        tmp = vsha256hq_u32(abcd, efgh, msg1);
-        efgh = vsha256h2q_u32(efgh, abcd, msg1);
-        abcd = tmp;
-
-        w2 = vsha256su0q_u32(w2, w3);
-        w2 = vsha256su1q_u32(w2, w0, w1);
-        msg2 = vaddq_u32(w2, (i==0?k24_27:(i==1?k40_43:k56_59)));
-        tmp = vsha256hq_u32(abcd, efgh, msg2);
-        efgh = vsha256h2q_u32(efgh, abcd, msg2);
-        abcd = tmp;
-
-        w3 = vsha256su0q_u32(w3, w0);
-        w3 = vsha256su1q_u32(w3, w1, w2);
-        msg3 = vaddq_u32(w3, (i==0?k28_31:(i==1?k44_47:k60_63)));
-        tmp = vsha256hq_u32(abcd, efgh, msg3);
-        efgh = vsha256h2q_u32(efgh, abcd, msg3);
-        abcd = tmp;
-    }
-
-    vst1q_u32(&state[0], vrev_u32(vaddq_u32(abcd, abcd_orig)));
-    vst1q_u32(&state[4], vrev_u32(vaddq_u32(efgh, efgh_orig)));
-}
+#endif
 
 static const uint32_t sha256_initial_state[] = {
     0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
@@ -143,19 +49,19 @@ void sha256sse_1B(uint32_t *i0, uint32_t *i1, uint32_t *i2, uint32_t *i3, uint8_
     uint32_t state[8];
     // i0
     memcpy(state, sha256_initial_state, 32);
-    sha256_armv8_block(state, i0);
+    sha256_block_fallback(state, i0);
     for(int i=0; i<8; i++) ((uint32_t*)d0)[i] = __builtin_bswap32(state[i]);
     // i1
     memcpy(state, sha256_initial_state, 32);
-    sha256_armv8_block(state, i1);
+    sha256_block_fallback(state, i1);
     for(int i=0; i<8; i++) ((uint32_t*)d1)[i] = __builtin_bswap32(state[i]);
     // i2
     memcpy(state, sha256_initial_state, 32);
-    sha256_armv8_block(state, i2);
+    sha256_block_fallback(state, i2);
     for(int i=0; i<8; i++) ((uint32_t*)d2)[i] = __builtin_bswap32(state[i]);
     // i3
     memcpy(state, sha256_initial_state, 32);
-    sha256_armv8_block(state, i3);
+    sha256_block_fallback(state, i3);
     for(int i=0; i<8; i++) ((uint32_t*)d3)[i] = __builtin_bswap32(state[i]);
 }
 
@@ -163,23 +69,23 @@ void sha256sse_2B(uint32_t *i0, uint32_t *i1, uint32_t *i2, uint32_t *i3, uint8_
     uint32_t state[8];
     // i0
     memcpy(state, sha256_initial_state, 32);
-    sha256_armv8_block(state, i0);
-    sha256_armv8_block(state, i0 + 16);
+    sha256_block_fallback(state, i0);
+    sha256_block_fallback(state, i0 + 16);
     for(int i=0; i<8; i++) ((uint32_t*)d0)[i] = __builtin_bswap32(state[i]);
     // i1
     memcpy(state, sha256_initial_state, 32);
-    sha256_armv8_block(state, i1);
-    sha256_armv8_block(state, i1 + 16);
+    sha256_block_fallback(state, i1);
+    sha256_block_fallback(state, i1 + 16);
     for(int i=0; i<8; i++) ((uint32_t*)d1)[i] = __builtin_bswap32(state[i]);
     // i2
     memcpy(state, sha256_initial_state, 32);
-    sha256_armv8_block(state, i2);
-    sha256_armv8_block(state, i2 + 16);
+    sha256_block_fallback(state, i2);
+    sha256_block_fallback(state, i2 + 16);
     for(int i=0; i<8; i++) ((uint32_t*)d2)[i] = __builtin_bswap32(state[i]);
     // i3
     memcpy(state, sha256_initial_state, 32);
-    sha256_armv8_block(state, i3);
-    sha256_armv8_block(state, i3 + 16);
+    sha256_block_fallback(state, i3);
+    sha256_block_fallback(state, i3 + 16);
     for(int i=0; i<8; i++) ((uint32_t*)d3)[i] = __builtin_bswap32(state[i]);
 }
 
@@ -187,19 +93,19 @@ void sha256sse_checksum(uint32_t *i0, uint32_t *i1, uint32_t *i2, uint32_t *i3, 
     uint32_t state[8];
     // i0
     memcpy(state, sha256_initial_state, 32);
-    sha256_armv8_block(state, i0);
+    sha256_block_fallback(state, i0);
     *((uint32_t *)d0) = __builtin_bswap32(state[0]);
     // i1
     memcpy(state, sha256_initial_state, 32);
-    sha256_armv8_block(state, i1);
+    sha256_block_fallback(state, i1);
     *((uint32_t *)d1) = __builtin_bswap32(state[0]);
     // i2
     memcpy(state, sha256_initial_state, 32);
-    sha256_armv8_block(state, i2);
+    sha256_block_fallback(state, i2);
     *((uint32_t *)d2) = __builtin_bswap32(state[0]);
     // i3
     memcpy(state, sha256_initial_state, 32);
-    sha256_armv8_block(state, i3);
+    sha256_block_fallback(state, i3);
     *((uint32_t *)d3) = __builtin_bswap32(state[0]);
 }
 
